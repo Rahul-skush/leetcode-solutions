eeafd6b01eecda3ea854f62743b91e6679435bce
Rahul Singh <43444047+Rahul-skush@users.noreply.github.com>
2021-02-15 15:38:56 +0530

    Create 322. Coin Change.cpp

---
+++ dynamic programming/322. Coin Change.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+       vector<int> dp(amount+1,1e7);
+        dp[0] = 0;
+        for(int i=0;i<=amount;i++)
+        {
+            for(int j =0;j<coins.size();j++)
+            {
+                if(i>=coins[j])
+                    dp[i] = min(dp[i-coins[j]] +1, dp[i]);
+            }
+        }
+        return dp[amount]>=1e7?-1:dp[amount];
+    }
+};
