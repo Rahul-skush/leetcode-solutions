a56aeb94c00bbc16c48c23f3b5744c4bfe10ecf5
Rahul Singh <43444047+Rahul-skush@users.noreply.github.com>
2021-02-15 16:36:45 +0530

    Create 413. Arithmetic Slices.cpp

---
+++ dynamic programming/413. Arithmetic Slices.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& A) {
+        int n = A.size();
+        int ans = 0,  cnt =0;
+        for(int i =2;i<A.size();i++)
+        {
+            if(A[i]-A[i-1] == A[i-1] - A[i-2])
+                cnt++;
+            else cnt =0;
+            ans += cnt;
+        }
+        return ans;
+    }
+};
