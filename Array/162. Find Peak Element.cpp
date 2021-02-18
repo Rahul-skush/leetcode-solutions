2bd3e403bbba9298d4847bd150836696fe6995d9
Rahul Singh <43444047+Rahul-skush@users.noreply.github.com>
2021-02-19 01:10:02 +0530

    Create 162. Find Peak Element.cpp

---
+++ Array/162. Find Peak Element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n = nums.size();
+        if(n<=1) return 0;
+        if(nums[0]>nums[1]) return 0;
+        for(int i =1;i<n-1;i++)
+        {
+            if(nums[i]>nums[i-1] && nums[i]>nums[i+1])
+                return i;
+        }
+        if(nums[n-1]>nums[n-2]) return n-1;
+        return -1;
+    }
+};
