11f2264cacbd76701eaa861b6a218227dcb3f860
Rahul Singh <43444047+Rahul-skush@users.noreply.github.com>
2021-03-13 00:05:05 +0530

    Create 275. H-Index II.cpp

---
+++ Array/275. H-Index II.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int left=0, len = citations.size(), right= len-1,  mid;
+        while(left<=right)
+        {
+            mid=(left+right)>>1;
+            if(citations[mid]== (len-mid)) return citations[mid];
+            else if(citations[mid] > (len-mid)) right = mid - 1;
+            else left = mid + 1;
+        }
+        return len - (right+1);
+    }
+};
