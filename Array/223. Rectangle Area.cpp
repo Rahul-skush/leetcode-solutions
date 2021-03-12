948b2cca470e59df967f0783bde095fc2784e525
Rahul Singh <43444047+Rahul-skush@users.noreply.github.com>
2021-03-13 00:46:21 +0530

    Create 223. Rectangle Area.cpp

---
+++ Array/223. Rectangle Area.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+        long total = (long)(C-A) *(long) (D-B) + (long)(G-E) * (long)(H-F);
+    
+    if (C<=E || A>=G || B>=H || D<=F )
+        return total;
+    else
+    {
+        vector <int> h;
+        h.push_back(A);
+        h.push_back(C);
+        h.push_back(E);
+        h.push_back(G);
+   
+        vector <int> v;
+        v.push_back(B);
+        v.push_back(D);
+        v.push_back(F);
+        v.push_back(H);
+    
+        sort(h.begin(), h.end());
+        sort(v.begin(), v.end());
+    
+        total = total - (h[2] - h [1]) * (v[2] - v[1]);
+        return total;
+    }
+        return 0;
+    }
+    
+};
