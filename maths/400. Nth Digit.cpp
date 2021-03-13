f3ee514dbe78a2cca194f6456f2ace2516f2efa0
Rahul Singh <43444047+Rahul-skush@users.noreply.github.com>
2021-03-13 13:37:14 +0530

    Create 400. Nth Digit.cpp

---
+++ maths/400. Nth Digit.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findNthDigit(int n) {
+        long base = 9, digits = 1;
+        while(n-base*digits>0)
+        {
+            n -= base*digits;
+            base*=10; digits++;
+        }
+        
+        int index = (n-1)%digits;
+        int offset = (n-1)/digits;
+        long start = pow(10, digits -1);
+        return to_string(start + offset)[index]- '0';
+    }
+};
