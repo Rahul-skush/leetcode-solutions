21d9a23313c2111e712883e1d19537514008282b
Rahul Singh <43444047+Rahul-skush@users.noreply.github.com>
2021-03-26 23:38:48 +0530

    Create 35. Search Insert Position.cpp

---
+++ Array/35. Search Insert Position.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l = 0, r = nums.size()-1;
+        while(l<=r)
+        {
+            int m = (l+r)/2;
+            if(nums[m]==target) return m;
+            if(nums[m]>target) r = m-1;
+            else l = m+1;
+        }
+        return l;
+    }
+};
