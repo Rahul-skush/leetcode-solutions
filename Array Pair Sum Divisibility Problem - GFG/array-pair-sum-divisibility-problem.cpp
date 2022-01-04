180f983f86e6a5b8e203d94345bcfb7e4b6c6531
Rahul Singh <43444047+Rahul-skush@users.noreply.github.com>
2022-01-04 15:01:40 +0530

    Added solution - LeetHub

---
+++ Array Pair Sum Divisibility Problem - GFG/array-pair-sum-divisibility-problem.cpp
@@ -0,0 +1,48 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+  public:
+    bool canPair(vector<int> nums, int k) {
+        // Code here.
+        if(k==1) return nums.size()%2==0;
+        unordered_map<int, int> mp;
+        for(auto x : nums)
+        mp[x%k]++;
+        
+        for(auto x : mp)
+        {
+            int one = x.first;
+            int two = k - x.first;
+            if(two==k) two =0;
+            if(one==two){
+                if(mp[one]%2) return 0;
+                else continue;
+            } 
+            if(mp[one]!=mp[two]) return 0;
+        }
+        
+        return 1;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n, k;
+        cin >> n >> k;
+        vector<int> nums(n);
+        for (int i = 0; i < nums.size(); i++) cin >> nums[i];
+        Solution ob;
+        bool ans = ob.canPair(nums, k);
+        if (ans)
+            cout << "True\n";
+        else
+            cout << "False\n";
+    }
+    return 0;
+}  // } Driver Code Ends
