1e243d7ec590a404664988d0b8b3944e543dd0e4
Rahul Singh <43444047+Rahul-skush@users.noreply.github.com>
2022-01-06 00:34:41 +0530

    Added solution - LeetHub

--- Nuts and Bolts Problem - GFG/nuts-and-bolts-problem.cpp
+++ Nuts and Bolts Problem - GFG/nuts-and-bolts-problem.cpp
@@ -12,10 +12,31 @@ public:
 	void matchPairs(char nuts[], char bolts[], int n) {
 	    // code here
 	    
+	    vector<char> st = {'!', '#', '$', '%', '&', '*', '@', '^', '~'};
 	    
+	    unordered_set<char> nut;
+	    for(int i=0;i<n;i++)
+	    nut.insert(nuts[i]);
 	    
-	    sort(nuts, nuts+n);
-	    sort(bolts, bolts+n);
+	     int i=0, j=0;
+	     while(i<9)
+	     {
+	         if(nut.find(st[i])!=nut.end())
+	         nuts[j++]= st[i];
+	         i++;
+	     }
+	     
+	     unordered_set<char> bolt;
+	    for(int i=0;i<n;i++)
+	    bolt.insert(bolts[i]);
+	    
+	      i=0, j=0;
+	     while(i<9)
+	     {
+	         if(bolt.find(st[i])!=bolt.end())
+	         bolts[j++]= st[i];
+	         i++;
+	     }
 	    
 	}
 
