e9c47bc24e8e9ee8ebc1025761664c7023df2b55
Rahul Singh <43444047+Rahul-skush@users.noreply.github.com>
2022-01-16 22:52:48 +0530

    Time: 28 ms (12.31%), Space: 22.1 MB (5.24%) - LeetHub

--- 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.cpp
+++ 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.cpp
@@ -12,40 +12,24 @@ public:
                 cnt =0;
             }else {cnt++;}
             right[i] =  {cnt, f} ;
-            
-            //cout<<right[i]<<" ";
         }
         
-        f =0; cnt =0;
-        for(int i=0;i<n;i++)
-        {
-            if(seats[i])
-            { 
-                f =1;
-                cnt =0;
-            }else {cnt++;}
-            left[i] =  {cnt, f} ;
-            
-            //cout<<right[i]<<" ";
-        }
         
-         cnt =0;
+        
+        cnt =0;
+        f =0;
              int ans =0;
         for(int i=0;i<n;i++)
         {
-            if(seats[i]) cnt =0;
+            if(seats[i]) {cnt =0; f =1;}
             else cnt++;
             
-            if(right[i][1]==0 || left[i][1]==0)
-            {
-                ans = max({ans, left[i][0], right[i][0]});
-               // cout<< max(left[i][0], right[i][0])<<" ";
-            }else
-            {ans = max(ans, min(left[i][0], right[i][0]));
-            //cout<< min(left[i][0], right[i][0])<<" ";
-            }
+            if(right[i][1]==0 || f==0)
+             ans = max({ans, cnt, right[i][0]});
+            else
+             ans = max(ans, min(cnt, right[i][0]));
+            
         }
-        //cout<<endl;
         return ans;
     }
 };
