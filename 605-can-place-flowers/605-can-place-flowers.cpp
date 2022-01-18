d55b50938cf622895252d31e1288440cda96bb14
Rahul Singh <43444047+Rahul-skush@users.noreply.github.com>
2022-01-18 10:41:16 +0530

    Time: 33 ms (14.96%), Space: 20.2 MB (69.55%) - LeetHub

--- 605-can-place-flowers/605-can-place-flowers.cpp
+++ 605-can-place-flowers/605-can-place-flowers.cpp
@@ -2,45 +2,22 @@ class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
         if(n==0) return 1;
-        int i =0, cnt =0, m = flowerbed.size(), ans =0;
-        while(i<m && flowerbed[i]==0)
-            {cnt++; i++;}
-        
-        if(cnt)
-        {
-            if(i<m) cnt-=1;
-            if(cnt>0)
-            ans = (cnt/2) + cnt%2;
-        }
-        if(i==m) return ans>=n;
-        int j = m-1;
-        cnt =0;
-        while(j>i && flowerbed[j]==0)
-            {cnt++; j--;}
-        if(j>=i)
-        {
-            cnt-=1;
-            if(cnt>0)
-            ans += cnt/2 + cnt%2;
-        }
-        if(j==-1) return ans>=n;
-        cnt=0;
-        while(i<j)
-        {
-            if(flowerbed[i])
+        int i =-2, j =0, m = flowerbed.size(), ans =0;
+        while(j<m)
             {
-                cnt -=2;
-                if(cnt>0)
-                ans += cnt/2 + cnt%2;
-                cnt =0;
-            }else cnt++;
-            i++;
-        }
+                if(flowerbed[j])
+                {
+                  int  cnt = j-i-1 -2;
+                    if(cnt>0)
+                       ans += (cnt/2) + cnt%2; 
+                    i=j;
+                }
+              j++;
+            }
+        int  cnt = j-i-1 -1;
+                    if(cnt>0)
+                       ans += (cnt/2) + cnt%2; 
         
-              cnt -=2;
-                if(cnt>0)
-                ans += cnt/2 + cnt%2;
-                cnt =0;
         //cout<<ans<<"\n";
         return ans>=n;
     }
