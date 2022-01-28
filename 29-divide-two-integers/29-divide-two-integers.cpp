7dcc00fca13a857adf9d8fc941861f6289cae938
Rahul Singh <43444047+Rahul-skush@users.noreply.github.com>
2022-01-28 19:02:43 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (74.97%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.cpp
+++ 29-divide-two-integers/29-divide-two-integers.cpp
@@ -4,17 +4,26 @@ public:
         if (dividend == INT_MIN && divisor == -1) {
             return INT_MAX;
         }
-        long dvd = labs(dividend), dvs = labs(divisor), ans = 0;
-        int sign = dividend > 0 ^ divisor > 0 ? -1 : 1;
-        while (dvd >= dvs) {
-            long temp = dvs, m = 1;
-            while (temp << 1 <= dvd) {
-                temp <<= 1;
-                m <<= 1;
+        long long dvd = labs(dividend), dvs = labs(divisor);
+        long long ans = 0;
+        int sign = dividend>0 ^ divisor>0?-1:1;
+        
+        while(dvd>=dvs)
+        {
+            long long tmp = dvs, m =1;
+            
+            while(tmp<=dvd)
+            {
+                tmp = tmp<<1;
+                m = m<<1;
             }
-            dvd -= temp;
+            
+            if(tmp>dvd) {tmp = tmp>>1, m = m>>1;}
+            
+            dvd -= tmp;
             ans += m;
         }
-        return sign * ans;
+        
+        return sign*ans;
     }
 };
