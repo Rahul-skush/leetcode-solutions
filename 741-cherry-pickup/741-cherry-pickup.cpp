bfc70fefa5769d1103fee006b1161bb91651e4fc
Rahul Singh <43444047+Rahul-skush@users.noreply.github.com>
2022-03-19 16:51:25 +0530

    Time: 532 ms (16.90%), Space: 66 MB (11.96%) - LeetHub

---
+++ 741-cherry-pickup/741-cherry-pickup.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int n, m;
+    int dp[60][60][60][60];
+    int cherryPickup(vector<vector<int>>& grid) {
+      n  = grid.size(), m = grid[0].size();
+        memset(dp, -1, sizeof(dp));
+        
+        long ans = find(grid, 0, 0, 0, 0);
+        if(ans<0) return 0;
+        return ans;
+    }
+    
+    long find(vector<vector<int>>& grid, int r1, int c1, int r2, int c2)
+    {
+        if(!isValid(r1, c1) || !isValid(r2, c2) || grid[r1][c1]==-1 || grid[r2][c2]==-1) return INT_MIN;
+        if(r1==n-1 && c1==m-1) return grid[r1][c1];
+        if(dp[r1][c1][r2][c2]!=-1) return dp[r1][c1][r2][c2];
+        long x = max({find(grid,r1 + 1, c1, r2+1, c2), find(grid, r1+1, c1, r2, c2+1),
+                   find(grid, r1, c1+1, r2+1, c2), find(grid, r1, c1+1, r2, c2+1)});
+        
+        if(c1==c2 && r1==r2) x += grid[r1][c1];
+        else x += grid[r1][c1] + grid[r2][c2];
+        
+        return  dp[r1][c1][r2][c2] =x;
+    }
+    
+    bool isValid(int i, int j)
+    {
+        return i>=0 && j>=0 && i<n && j<m;
+    }
+};
