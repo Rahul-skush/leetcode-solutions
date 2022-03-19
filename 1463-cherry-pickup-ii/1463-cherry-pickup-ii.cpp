f3376556fcac634bedc9e4134d77a74a9350a9d7
Rahul Singh <43444047+Rahul-skush@users.noreply.github.com>
2022-03-19 16:04:15 +0530

    Time: 52 ms (84.23%), Space: 10.6 MB (61.61%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -5,22 +5,47 @@ public:
     int cherryPickup(vector<vector<int>>& grid) {
       n  = grid.size(), m = grid[0].size();
         memset(dp, -1, sizeof(dp));
+        int ans =0;
+        dp[0][0][m-1] = grid[0][0] + grid[0][m-1];
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                for(int k=0;k<m;k++)
+                {
+                    if(dp[i-1][j][k]>=0){
+                    
+                        for(auto d1 : dir)
+                        {
+                            int c1 = d1 + j;
+                            for(auto d2 : dir)
+                            {
+                                int c2 = d2 + k;
+                                if(c1>=0 && c2>=0 && c1<m && c2<m)
+                                {
+                                    dp[i][c1][c2] = max(dp[i][c1][c2], 
+                                                     dp[i-1][j][k] +
+                                (c1==c2?grid[i][c1]:(grid[i][c1] + grid[i][c2])));
+                                 
+                                 
+                                if(i==n-1)
+                                ans = max(dp[i][c1][c2], ans);
+                                
+                                }
+                            }
+                        }
+                        
+                    }
+                    
+                    
+                }
+            }
+           
+        }
         
-        return find(grid, 0, m-1, 0);
+        return ans;
     }
     
-    int find(vector<vector<int>>& grid, int c1, int c2, int r)
-    {
-        if(c1<0 || c2<0 || c1>=m || c2>=m) return INT_MIN;
-        if(r==n) return 0;
-        if(dp[r][c1][c2]!=-1) return dp[r][c1][c2];
-        int x = max({find(grid, c1, c2+1, r+1), find(grid, c1, c2-1, r+1), find(grid, c1, c2, r+1),
-                   find(grid, c1-1, c2+1, r+1), find(grid, c1-1, c2-1, r+1), find(grid, c1-1, c2, r+1),
-                   find(grid, c1+1, c2+1, r+1), find(grid, c1+1, c2-1, r+1), find(grid, c1+1, c2, r+1)});
-        
-        if(c1==c2) x += grid[r][c1];
-        else x += grid[r][c1] + grid[r][c2];
-        
-        return  dp[r][c1][c2] =x;
-    }
+    vector<int> dir = {-1, 0, 1};
+    
 };
