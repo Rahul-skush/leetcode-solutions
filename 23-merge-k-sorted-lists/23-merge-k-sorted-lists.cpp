6683b86d3b771f3decbdb1d3557e48cfc70ea023
Rahul Singh <43444047+Rahul-skush@users.noreply.github.com>
2022-03-20 01:49:30 +0530

    Time: 16 ms (98.84%), Space: 14.2 MB (11.55%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -1,31 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    vector<int> v;
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        findNodes(lists);
-        int flag =0, i=0;
-        sort(v.begin(), v.end());  // sorting takes nlogn
-        ListNode* ans=new ListNode(INT_MIN), *node = new ListNode(0); // initializing nodes with dummy data, to check null lists.
-        while(i<v.size())
-        { 
-        ListNode* curr = new ListNode(v[i++]);
-        node->next = curr;
-        node = node->next;
-             if(!flag) {ans = node; flag =1;}
+        priority_queue<pair<int, ListNode*>, vector<pair<int, ListNode*>>,
+                    greater<pair<int, ListNode*>>> pq;
+        for(auto x : lists)
+            if(x)pq.push({x->val, x});
+        
+        ListNode *dummy = new ListNode(-1);
+        ListNode * ans = dummy;
+        
+        while(!pq.empty())
+        {
+            pair<int, ListNode*> p = pq.top(); pq.pop();
+            dummy->next = new ListNode(p.first);
+            if(p.second->next)
+            {
+                pq.push({p.second->next->val, p.second->next});
+            }
+            
+            dummy = dummy->next;
         }
         
-        return ans->val == INT_MIN?NULL: ans; //if lists is null simply return NULL
-    }
-    
-    void findNodes(vector<ListNode*> &lists)
-    {
-        for(int i=0;i<lists.size();i++)
-        {
-            while(lists[i])
-            {
-               v.push_back(lists[i]->val);
-               lists[i] = lists[i]->next;
-            }
-        }
+        return ans->next;
+            
     }
 };
