c59d7a815374a69cc0cb4f12d50981d1cb8a9ef9
Rahul Singh <43444047+Rahul-skush@users.noreply.github.com>
2022-04-22 00:05:45 +0530

    Time: 135 ms (57.19%), Space: 49.8 MB (33.76%) - LeetHub

---
+++ 705-design-hashset/705-design-hashset.cpp
@@ -0,0 +1,35 @@
+class MyHashSet {
+private:
+	int prime;
+	vector<list<int>> table;
+
+	int hash(int key) {
+		return key % prime;
+	}
+
+	list<int>::iterator search(int key) {
+		int h = hash(key);
+		return find(table[h].begin(), table[h].end(), key);
+	}
+
+public:
+	MyHashSet() : prime(10007), table(prime) {}
+	
+	void add(int key) {
+		int h = hash(key);
+		if (!contains(key))
+			table[h].push_back(key);
+	}
+	
+	void remove(int key) {
+		int h = hash(key);
+		auto it = search(key);
+		if (it != table[h].end())
+			table[h].erase(it);
+	}
+	
+	bool contains(int key) {
+		int h = hash(key);
+		return search(key) != table[h].end();
+	}
+};
