1b97279150a1e037428f6ba43faa51b470362355
Rahul Singh <43444047+Rahul-skush@users.noreply.github.com>
2022-05-09 14:14:28 +0530

    Time: 6 ms (47.59%), Space: 8.1 MB (14.47%) - LeetHub

--- 214-shortest-palindrome/214-shortest-palindrome.cpp
+++ 214-shortest-palindrome/214-shortest-palindrome.cpp
@@ -3,23 +3,24 @@ public:
     string shortestPalindrome(string s) {
         string str = s;
         reverse(str.begin(), str.end());
-        str = s + '#' + str;
+        str = s + '$' + str;
         int n = str.size();
-        vector<int>lps(n, 0);
-        
-        int i=1;
-        while(i<n)
+        vector<int> lps(n);
+        int i =1;
+        while(i<str.size())
         {
-           int j = lps[i-1];
-            while(j>0 && str[i]!=str[j])
+            int j = lps[i-1];
+            while(j>0 && str[j]!=str[i])
                 j = lps[j-1];
-            
-            if(str[i]==str[j])
+            if(str[j]==str[i])
                 lps[i] = j+1;
             i++;
         }
+        
         str = s.substr(lps[n-1]);
         reverse(str.begin(), str.end());
-        return str + s;
+        str = str + s;
+        return str;
+        
     }
 };
