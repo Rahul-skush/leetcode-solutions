411e801e4aef60851ab5d161a01e59483823bc61
Rahul Singh <43444047+Rahul-skush@users.noreply.github.com>
2022-07-24 19:08:40 +0530

    Time: 31 ms (30.62%), Space: 11 MB (19.49%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        vector<int> dp(1002, 0);
+        map<int, int> mp;
         for(auto x : trips)
         {
-            dp[x[1]] += x[0];
-            dp[x[2]] -= x[0];
-        }
-        int sum =0;
-        for(int i=0;i<1002;i++)
-        {
-            sum += dp[i];
-            if(sum>capacity) return 0;
+            mp[x[1]] += x[0];
+            mp[x[2]] -= x[0];
         }
         
+        int sum =0;
+        for(auto x : mp)
+        {
+            sum += x.second;
+            if(sum>capacity) return 0;
+        }
         return 1;
     }
 };
