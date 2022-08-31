54d58b83fa86a75a5851c4ee5d39fa2d8d6c79e9
Rahul Singh <43444047+Rahul-skush@users.noreply.github.com>
2022-08-31 11:54:42 +0530

    Time: 178 ms (14.31%), Space: 28.6 MB (12.54%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -1,61 +1,51 @@
 class Solution {
 public:
-    int n, m, f1=0, f2=0;
-    set<vector<int>> st;
-    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights)     {
-        n = heights.size(), m =heights[0].size();
-        vector<vector<int>> vis1(n, vector<int>(m, 0));
-        vector<vector<int>> vis2(n, vector<int>(m, 0));
-        queue<vector<int>> q;
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        n = heights.size(), m = heights[0].size();
+        vector<vector<int>> mat1(n, vector<int>(m, 0)), mat2(n, vector<int>(m, 0));
         for(int i=0;i<n;i++)
-            {q.push({i, 0}); vis1[i][0]=1;}
-        for(int i=0;i<m;i++)
-            {q.push({0, i}); vis1[0][i]=1;}
-        
-        while(!q.empty())
         {
-            vector<int> v = q.front(); q.pop();
-            for(auto d : dirs)
-            {
-                int i = v[0] + d[0], j = v[1]+d[1];
-                if(isValid(i, j) && heights[i][j]>=heights[v[0]][v[1]] 
-                   && vis1[i][j]==0)
-                    {q.push({i, j}); vis1[i][j]=1;}
-            }
+            dfs(i, 0, heights, mat1);
+            dfs(i, m-1, heights, mat2);
         }
         
         for(int i=0;i<m;i++)
-            {q.push({n-1, i}); vis2[n-1][i]=1;}
-        for(int i=0;i<n;i++)
-            {q.push({i, m-1}); vis2[i][m-1]=1;}
-        
-        while(!q.empty())
         {
-            vector<int> v = q.front(); q.pop();
-            for(auto d : dirs)
-            {
-                int i = v[0] + d[0], j = v[1]+d[1];
-                if(isValid(i, j) && heights[i][j]>=heights[v[0]][v[1]] 
-                   && vis2[i][j]==0)
-                    {q.push({i, j}); vis2[i][j]=1;}
-            }
+            dfs(0, i, heights, mat1);
+            dfs(n-1, i, heights, mat2);
         }
         
-        vector<vector<int>> v;
+        
+        vector<vector<int>> res;
+        
+        
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
-                if(vis1[i][j] + vis2[i][j]==2)
-                    v.push_back({i, j});
+            {
+                    if(mat1[i][j] + mat2[i][j]==2) res.push_back({i, j});
+            }
         }
-        
-        return v;
+        return res;
     }
-    
-    vector<vector<int>> dirs = {{0,1}, {0, -1}, {1, 0}, {-1, 0}};
-    
-    bool isValid(int i, int j)
+    private:
+    int n, m;
+    bool isValid(int i, int j, int x, int y, vector<vector<int>>& heights, vector<vector<int>>& mat)
     {
-        return i>=0 && j>=0 && i<n && j<m;
+        if(x<0 || y<0 || x>=n || y>=m || mat[x][y]==1 || heights[x][y]<heights[i][j]) return 0;
+        return 1;
     }
+    
+    void dfs(int i, int j, vector<vector<int>>& heights, vector<vector<int>>& mat)
+    {
+        mat[i][j] =1;
+        for(auto d : dirs)
+        {
+            int x = d[0] + i, y = d[1] + j;
+            if(isValid(i, j, x, y, heights, mat))
+                dfs(x, y, heights, mat);
+        }
+    }
+    
+    vector<vector<int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 };
