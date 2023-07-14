eaae032d2eacc646c12685ee83698a5b5398120a
Rahul Singh <43444047+Rahul-skush@users.noreply.github.com>
2023-07-14 12:49:35 +0530

    Time: 4 ms (41.73%), Space: 6.1 MB (44.70%) - LeetHub

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
@@ -1,38 +1,34 @@
 class Solution {
 public:
     bool isRobotBounded(string instructions) {
-        
         string s = instructions;
-        s += instructions;
-        s += instructions;
-        s += instructions;
         
-        vector<int> v = {0, 0, 1};
+        int i =0, j =0, dir = 0;
         
-        for(auto c : s)
+        for(int k =0;k<=6;k++)
         {
-            int d = v[2];
-            if(c=='G')
-            {
-                if(d==1) v = {v[0], v[1]+1, d};
-                if(d==2) v = {v[0]+1, v[1], d};
-                if(d==3) v = {v[0], v[1]-1, d};
-                if(d==4) v = {v[0]-1, v[1], d};
-            }else if(c=='L'){
-                if(d==1) v = {v[0], v[1], 4};
-                if(d==2) v = {v[0], v[1], 1};
-                if(d==3) v = {v[0], v[1], 2};
-                if(d==4) v = {v[0], v[1], 3};
-            } else {
-                if(d==1) v = {v[0], v[1], 2};
-                if(d==2) v = {v[0], v[1], 3};
-                if(d==3) v = {v[0], v[1], 4};
-                if(d==4) v = {v[0], v[1], 1};
+        for(auto x: s)
+        {
+            if(x == 'L')
+                dir = (dir + 4 -1)%4;
+            else if(x=='R')
+                dir = (dir + 4 +1)%4;
+            else {
+                if(dir==0)
+                    j += 1;
+                else if(dir==1)
+                    i +=1;
+                else if(dir==2)
+                    j-=1;
+                else if(dir==3)
+                    i-=1;
             }
-            
-            
-        }
-        if(v[0]==0 && v[1]==0) return 1;
-        return 0;
+        } 
+         if(i==0 && j==0) return true;
     }
+        
+        return false;
+    }
+    
+    
 };
