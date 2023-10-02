178454948b8e3e9a98e68ed5c84ab5a781b63153
Rahul Singh <43444047+Rahul-skush@users.noreply.github.com>
2023-10-02 11:32:37 +0530

    Time: 37 ms (33.01%), Space: 13.6 MB (23.75%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     bool winnerOfGame(string colors) {
-        int cnta =0, cntb = 0, n = colors.size();
-        for(int i=0;i<n;)
-        {
-            int cnt=0;
-            while(i<n && colors[i]=='A') {cnt++; i++;}
-            
-            if(cnt>=3)
-            cnta += cnt-2;
-            i++;
-        }
+        int cnta =0, cntb = 0;
+        int i = 0;
         
-        for(int i=0;i<n;)
+        while(i<colors.size())
         {
-            int cnt=0;
-            while(i<n && colors[i]=='B') {cnt++; i++;}
-            
-            if(cnt>=3)
-            cntb += cnt-2;
-            i++;
+            char c = colors[i];
+            int cnt =0;
+            while(i<colors.size() && colors[i]==c)
+                {cnt+=1; i++;}
+            if(cnt<=2) continue;
+            if(c=='A')
+                cnta += cnt-2;
+            else cntb += cnt-2;
         }
         
         return cnta>cntb;
