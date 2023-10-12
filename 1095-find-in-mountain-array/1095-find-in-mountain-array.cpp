137a2181d4075522035ed6c9b5c82b1b17cf2806
Rahul Singh <43444047+Rahul-skush@users.noreply.github.com>
2023-10-12 23:18:34 +0530

    Time: 6 ms (44.79%), Space: 7.3 MB (27.99%) - LeetHub

--- 1095-find-in-mountain-array/1095-find-in-mountain-array.cpp
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.cpp
@@ -11,60 +11,61 @@
 class Solution {
 public:
     int findInMountainArray(int target, MountainArray &mountainArr) {
-        if(mountainArr.length()<3) return -1;
-        int peak = findPeak(mountainArr);
-        //cout<<peak<<" ";
-        int left = findleft(mountainArr, 0, peak, target);
+        
+        int peak = findPeak(target, mountainArr);
+        if(peak==-1) return -1;
+        int left =findLeft(target, mountainArr, 0, peak);
         if(left!=-1) return left;
-        int right = findright(mountainArr, peak+1, mountainArr.length()-1, target);
+        int right =findRight(target, mountainArr, peak, mountainArr.length()-1);
         if(right!=-1) return right;
         
         return -1;
     }
     
-    int findleft(MountainArray &arr, int l, int h, int target)
+    int findLeft(int target,  MountainArray &mountainArr, int l, int r)
     {
-        while(l<h)
+        while(l<r)
         {
-            int m = l + (h-l)/2;
-            int x = arr.get(m);
+            int m = (l+r)/2;
+            int x = mountainArr.get(m);
             if(x==target) return m;
-            if(x>target) h =m;
-            else  l = m+1;
+            if(x>target) r = m;
+            else l = m+1;
         }
-        if(arr.get(l)==target) return l;
+        if(mountainArr.get(l)==target) return l;
         return -1;
     }
     
-    int findright(MountainArray &arr, int l, int h, int target)
+    int findRight(int target,  MountainArray &mountainArr, int l, int r)
     {
-        while(l<h)
+        while(l<r)
         {
-            int m = l + (h-l)/2;
-            int x = arr.get(m);
+            int m = (l+r)/2;
+            int x = mountainArr.get(m);
             if(x==target) return m;
-            if(x<target) h =m;
-            else  l = m+1;
+            if(x<target) r = m;
+            else l = m+1;
         }
-        if(arr.get(l)==target) return l;
+        if(mountainArr.get(l)==target) return l;
         return -1;
     }
     
-    int findPeak(MountainArray &arr)
+    int findPeak(int target, MountainArray &mountainArr)
     {
-        int l = 0, n = arr.length()-1;
-        int h = n;
+        int l = 0, h = mountainArr.length()-1;
+        
         while(l<h)
         {
-            int m = l + (h-l)/2;
-            int x = arr.get(m);
-            int y =INT_MAX;
-            if(m-1>=0) y = arr.get(m-1);
-            if(m-1>=0 && m+1<=n && x>y && x>arr.get(m+1)) return m;
-            if(m-1>=0 && x>y) l =m+1;
+            int m = (l+h)/2;
+            int x = mountainArr.get(m);
+            int y = INT_MIN, z = INT_MAX;
+            if(m-1>=0) y = mountainArr.get(m-1);
+            if(m+1<=h) z = mountainArr.get(m+1);
+            if(x>y && x>z) return m;
+            if(x>y) l = m+1;
             else h = m;
         }
         
-        return l;
+        return -1;
     }
 };
