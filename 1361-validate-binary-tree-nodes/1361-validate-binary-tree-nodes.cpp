4110f272bfa3d181a79a34eb1df5870e214e22aa
Rahul Singh <43444047+Rahul-skush@users.noreply.github.com>
2023-10-17 23:58:52 +0530

    Time: 33 ms (79.63%), Space: 33.1 MB (64.29%) - LeetHub

--- 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.cpp
+++ 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.cpp
@@ -1,27 +1,40 @@
 class Solution {
 public:
     bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) {
-        vector<int> v(n, 0);
-        int cnt =0, root=-1;
+        vector<int> indegree(n, 0);
         for(int i=0;i<n;i++)
         {
-            if(leftChild[i]!=-1) v[leftChild[i]]++;
-            if(rightChild[i]!=-1) v[rightChild[i]]++;
+            if(leftChild[i]!=-1) indegree[leftChild[i]] += 1;
+            if(rightChild[i]!=-1) indegree[rightChild[i]] += 1;
+        }
+        
+        int val = -1;
+        for(int i=0;i<n;i++)
+            if((indegree[i]==0 && val!=-1) || indegree[i]>1) return false;
+            else if(indegree[i]==0) val = i;
+        
+        if(val==-1) return false;
+        
+        vector<int> vis(n, 0);
+        queue<int> q;
+        q.push(val);
+        while(!q.empty())
+        {
+            int m = q.size();
+            for(int i=0;i<m;i++)
+            {
+                int p = q.front(); q.pop();
+                if(vis[p]) return false;
+                vis[p] = true;
+                if(leftChild[p]!=-1) q.push(leftChild[p]);
+                if(rightChild[p]!=-1) q.push(rightChild[p]);
+            }
         }
         
         for(int i=0;i<n;i++)
-            if(v[i]>1) return 0;
-        for(int i=0;i<n;i++)
-            if(v[i]==0) {cnt++; root = i;}
-        if(cnt>1 || root==-1) return 0;
-        
-        return find(leftChild, rightChild, root)==n;
-    }
-    
-    int find(vector<int> &l, vector<int> &r, int root)
-    {
-        if(root==-1) return 0;
-        
-        return 1 + find(l, r, l[root]) + find(l, r, r[root]);
+        {
+            if(vis[i]==0) return false;
+        }
+       return true;;
     }
 };
