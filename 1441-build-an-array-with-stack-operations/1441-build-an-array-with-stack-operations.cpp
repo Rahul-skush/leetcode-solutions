dd320866c65efe97f86feeba40a7cda0bdc84c93
Rahul Singh <43444047+Rahul-skush@users.noreply.github.com>
2023-11-03 12:17:40 +0530

    Time: 4 ms (39.00%), Space: 8.3 MB (22.92%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
@@ -5,13 +5,10 @@ public:
         int j =0, m = target.size();
         for(int i=1;i<=n && j<m;i++)
         {
-            if(target[j]==i){
-                j++;
-                res.push_back("Push");
-            }else{
-                res.push_back("Push");
+            res.push_back("Push");
+            if(target[j]!=i)
                 res.push_back("Pop");
-            }
+            else j++;
         }
         return res;
     }
