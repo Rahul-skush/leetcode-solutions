93cf3730a9b3bee5a227034ca14f4cfbced300e1
Rahul Singh <43444047+Rahul-skush@users.noreply.github.com>
2024-01-24 22:26:42 +0530

    Time: 259 ms (75.87%), Space: 177.8 MB (98.12%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -13,30 +13,31 @@ class Solution {
 public:
     int ans =0;
     int pseudoPalindromicPaths (TreeNode* root) {
-        if(!root) return 0;
-        map<int, int> mp;
+       map<int, int> mp;
         find(root, mp);
         return ans;
     }
     
-    bool help(map<int, int> &mp1)
-    {
-        int cnt =0;
-        for(auto x : mp1)
-            if(x.second&1) cnt++;
-        return cnt<=1;
-    }
-    
     void find(TreeNode * root, map<int, int> &mp)
     {
-    if(!root) return ;
-        mp[root->val]++;
-        if(!root->left && !root->right)
-        {
-            if(help(mp)) ans++;
+        if(!root) return ;
+        if(!root->left && !root->right) {
+            mp[root->val]++;
+            if(check(mp)) ans++;
+            mp[root->val]--;
+                   return ;
         }
+       mp[root->val]++;
         find(root->left, mp);
         find(root->right, mp);
-        mp[root->val]--;
+       mp[root->val]--;
+    }
+    
+    int check(map<int, int> &mp)
+    {
+        int cnto=0;
+        for(auto x :  mp)
+            if(x.second%2) cnto++;
+        return cnto<=1;
     }
 };
